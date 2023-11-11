aec95cea32683cca2a7a0fdb4f3a5405cd818a29
brandon narine <bnarine@uwaterloo.ca>
2023-11-11 00:01:57 -0500

    addded drop token

--- Connect_4_Robot.c
+++ Connect_4_Robot.c
@@ -1,8 +1,4 @@
-// Configure Sensors
-
-
 /*
-
 motor descriptions:
 
 motor A = left pulley
@@ -10,7 +6,6 @@ Motor B = flipper
 Motor C1 = spinner at the top 
 Motor C2 = spinner at the bottom (with sorter)
 Motor D = Right pulley
-
 */
 //Function Prototypes
 void playGame();
@@ -20,14 +15,18 @@ void moveSelect();
 void legalCheck();
 void robotMove();
 void moveCalc();
-void dropTocken();
+void dropToken();
 void sortToken();
 void reset();
 void sensorConfig();
 void gameState();
+
+
+
 //Main function
 task main()
 {
+	int currentCol = 0;
 	sensorConfig();
 	configureMotors();
 	while(SensorValue[S1] = false){}
@@ -76,8 +75,18 @@ void sensorConfig() {
 	SensorMode[S3] = modeEV3Color_Color;
 	wait1Msec(50);
 }
+
 void gameState()
 {
 
 
+}
+
+void dropToken(int currentCol, int colWant)
+{
+	int col_x_axis_Displacement= 0;
+	col_x_axis_Displacement = colWant-currentCol;
+	col_x_axis_Displacement*= DISTANCE_BETWEEN_TWO_COL;
+	col_hypotnuse_Displacement = col_x_axis_Displacement / cos(0.08726646) // which is angle is rad
+	m
 }
