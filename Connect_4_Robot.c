8415bb80bd377cb3bdfb482b8a9a6b1cc0372791
DanielRolinsky <danielrolinsky@gmail.com>
2023-11-10 21:05:53 -0500

    Create Connect_4_Robot.c

---
+++ Connect_4_Robot.c
@@ -0,0 +1,20 @@
+// Configure Sensors
+
+//Main function
+task main()
+{
+
+}
+
+//Playing Function
+
+	//HumanMove Function
+		//Legal Move check function
+		//Drop Token Function
+
+	//RobotMove Function
+		//MiniMax Function
+		//Drop Token Function
+
+//Sorting Function
+	//
