c88d529ee5d5356f8522b065112bbb5b969aa6f4
DanielRolinsky <danielrolinsky@gmail.com>
2023-11-10 21:46:47 -0500

    Function Prototypes

--- Connect_4_Robot.c
+++ Connect_4_Robot.c
@@ -1,24 +1,41 @@
 // Configure Sensors
+void sensorConfig() {
 	SensorType[S1] = sensorEV3_Touch;
-	SensorType[S2] = sensorEV3_Ultrasonic;
 	SensorType[S3] = sensorEV3_Color;
 	wait1Msec(50);
 	SensorMode[S3] = modeEV3Color_Color;
 	wait1Msec(50);
-// testing for github
+}
+
+//Function Prototypes
+void playGame();
+
+void humanMove();
+void moveSelect();
+void legalCheck();
+
+void robotMove();
+void moveCalc();
+
+
+void dropTocken();
+
 //Main function
 task main()
 {
+	while(SensorValue[S1] = false){}
+
 
 }
 
 //Playing Function
 
 	//HumanMove Function
+		//moveSelect
 		//Legal Move check function
 		//Drop Token Function
 
-	//RobotMove Function`
+	//RobotMove Function
 		//MiniMax Function
 		//Drop Token Function
 
