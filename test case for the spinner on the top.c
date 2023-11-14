18e5e64c23db5d626af7c49be89682c8069f142e
brandon narine <bnarine@uwaterloo.ca>
2023-11-13 19:10:12 -0500

    Update test case for the spinner on the top.c

--- test case for the spinner on the top.c
+++ test case for the spinner on the top.c
@@ -1,31 +1,36 @@
 void spinnerMotor(bool isHumanPlaying);
+void configureMotors();
+
 
 task main()
 {
+configureMotors();
+for(int i = 0; i <= 10; i ++){
 spinnerMotor(true);
+wait1Msec(1000);
+spinnerMotor(false);
 }
 
-void spinnerMotor(bool isHumanPlaying) // at the top!!!!!!
-{
-	while(!getButtonPress(buttonEnter))
+if(isHumanPlaying)
 	{
 
-		if(getButtonPress(buttonRight))
-		{
-				motor[motorC] = 25;
-				while (nMotorEncoder[motorC] < 45)
-				{}
-				motor[motorC] = 0;
-		}
-		else if(getButtonPress(buttonLeft))
-		{
-			motor[motorC] = -25;
-			while (nMotorEncoder[motorC] < 45)
-			{}
-			motor[motorC] = 0;
-		}
+		motor[motorC] = 25;
+		while (nMotorEncoder[motorC] < 45)
+		{}
+		motor[motorC] = 0;
 	}
-	while(getButtonPress(buttonEnter))
-	{}
+	else
+	{
 
+		motor[motorC] = -25;
+		while (nMotorEncoder[motorC1] > -45)
+		{}
+		motor[motorC] = 0;
+	}
+
+	void configureMotors()
+{
+	nMotorEncoder[motorA] = nMotorEncoder[motorB] = nMotorEncoder[motorC] = nMotorEncoder[motorD] = 0;
+	wait1Msec(500);
+	motor[motorA] = motor[motorB] = motor[motorC] = motor[motorD]= 0 ;
 }
