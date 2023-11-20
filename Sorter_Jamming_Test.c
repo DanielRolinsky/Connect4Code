f5fa0a226574638473e5557be945924cba5601da
brandon narine <bnarine@uwaterloo.ca>
2023-11-19 22:30:13 -0500

    xss

--- Sorter_Jamming_Test.c
+++ Sorter_Jamming_Test.c
@@ -1,5 +1,7 @@
+
+#include "mindsensors-motormux.h"
 void sortTokens(){
-	motor[motorC] = -10;
+	MSMMotor(mmotor_S4_1, -1);
 	wait1Msec(100);
 	bool tokens = true;
 	time1[T1] = 0;
@@ -9,14 +11,14 @@ void sortTokens(){
 
 	while(tokens && SensorValue[S1] == false){
   		if(SensorValue[S2] == 4) {
-  			motor[motorB] = -20;
-  			while(nMotorEncoder[motorB] > -65){}
+  			motor[motorB] = -35;
+  			while(nMotorEncoder[motorB] > -55){}
   			motor[motorB] = 0;
 		}
 
 		else if(SensorValue[S2] == 5) {
 	 		wait1Msec(500);
-	    	motor[motorB] = 20;
+	    	motor[motorB] = 35;
   			while(nMotorEncoder[motorB] < 0){}
   			motor[motorB] = 0;
 		}
@@ -25,49 +27,28 @@ void sortTokens(){
 
 		if(time1[T1] >= 1000){
 
-				if(previousMotor == nMotorEncoder[motorC]){
+				if(previousMotor == MSMMotorEncoder(mmotor_S4_1)){
 				  	displayString(4, "yes");
-						motor[motorC] = 10;
+						MSMMotor(mmotor_S4_1, 1);
 						wait1Msec(1000);
-						motor[motorC] = -10;
+						MSMMotor(mmotor_S4_1, -1);
 						}
-						previousMotor = nMotorEncoder[motorC];
+						previousMotor = MSMMotorEncoder(mmotor_S4_1);
 						displayString(5, "%f", previousMotor);
 						time1[T1] =0;
 					}
     }
 
-
-		/*if(nMotorEncoder[motorA] == nextEncoder){
-			time1[T2] = 0;
-			while(time1[T2] <= 1000){
-				motor[motorA] = 10;
-			}
-			motor[motorA] = -10;
-		}
-
-		nextEncoder = nMotorEncoder[motorA] + 2;
-
-
-		if(nMotorEncoder[motorA] == previousMotor){
-			time1[T2] = 0;
-			while(time1[T2] <= 1000){
-				motor[motorA] = 10;
-			}
-			motor[motorA] = -10;
-		}
-
-		if(time1[T1] == 250){
-			previousMotor = nMotorEncoder[motorA];
-			time1[T1] = 0;
-		}*/
-	motor[motorB] = 20;
-  	while(nMotorEncoder[motorB] < 0)
+	motor[motorB] = 50;
+  	while(nMotorEncoder[motorB] < -10)
   		{}
+  	MSMotorStop(mmotor_S4_1);
   	motor[motorB] = 0;
 }
 
 task main(){
+	MSMMUXinit();
+	SensorType[S4] = sensorI2CCustom;
 	SensorType[S1] = sensorEV3_Touch;
 wait1Msec(50);
 SensorType[S2] = sensorEV3_Color;
@@ -75,7 +56,7 @@ wait1Msec(50);
 SensorMode[S2] = modeEV3Color_Color;
 wait1Msec(50);
 
-nMotorEncoder[motorC] = 0;
+MSMMotorEncoderReset(mmotor_S4_1);
 nMotorEncoder[motorB] = 0;
 
 
